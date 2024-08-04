5685863725f5ba2ce47c06b80fa814cedd8333af
JessYev97 <jessmyeverino@gmail.com>
2024-08-03 20:50:59 -0500

    write logic for grid

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.getElementById("Container").textContent = "test";
+
+const container = document.getElementById("container");
+/* let i=0, n=16;
+
+container.innerHTML = `<div class="rows">${'<div class ="cell">X</div>'.repeat(n)}</div>`.repeat(n).replace(/X/g,_=> (i++).toString(n)) 
+*/ 
+const gridSize = 16;
+
+for (let i = 0; i < gridSize; i++) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+
+  for(let j = 0; j < gridSize; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+  }
+  container.appendChild(row);
+}
+ 
